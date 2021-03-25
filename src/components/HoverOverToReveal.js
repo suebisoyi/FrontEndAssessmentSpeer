e8fbc662fc7cd77c1a24c2ccbd3ebd86c203cd7f
SudikshyaBisoyi <bisyoi@sheridancollege.ca>
2021-03-24 23:22:20 -0400

    Updates

--- src/components/HoverOverToReveal.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-
-$(document).ready(function() {
-    var $magic = $(".hiddenBackgroundRevealedOnMouseOver"),
-        magicWHalf = $magic.width() / 2;
-    $(document).on("mousemove", function(e) {
-      $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
-    });
-  });
